a02468a6b1fb4c810498649198365ea53de6bdbd
s-noojeenseng <6210110364@psu.ac.th>
2021-07-11 19:10:55 +0700

    continue finish lab

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import CharacterCard from './CharacterCard';
+
 import WordCard from './WordCard';
 
-const word = "Hello";
 function App() {
   return (
-    <div className="card">
+    <div>
       {
-        <WordCard value = "hello"/>
+        <WordCard value="hello" />
       }
     </div>
   );
 }
 
 export default App;
-
-//test commitxdxdcat1
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import _ from 'lodash';
+
 import CharacterCard from './CharacterCard';
-import _ from 'loadash';
 
 const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
@@ -14,17 +15,38 @@ const prepareStateFromWord = (given_word) => {
     }
 }
 
-const activationHandler = (c) => {
-    console.log(`${c} has been activated.`)
-    let guess = state.guess + c
-    setState({ ...state, guess })
-    if (guess.length == state.word.length) {
-        if (guess == state.word) {
-            console.log('yeah!')
-            setState({ ...state, guess: '', completed: true })
-        } else {
-            console.log('reset')
-            setState({ ...state, guess: '', attempt: state.attempt + 1 })
+
+export default function WordCard(props) {
+
+    const [state, setState] = useState(prepareStateFromWord(props.value))
+
+    const activationHandler = (c) => {
+        console.log(`${c} has been activated.`)
+
+        let guess = state.guess + c
+        setState({ ...state, guess })
+        
+        if (guess.length == state.word.length) {
+            if (guess == state.word) {
+                console.log('yeah!')
+                setState({ ...state, completed: true })
+            }
+            else {
+                console.log('reset, next attempt')
+                setState({ ...state, guess: '', attempt: state.attempt + 1 })
+            }
+            console.log(guess)
         }
     }
+
+    return (
+        <div>
+            {
+                state.chars.map((c, i) =>
+                    <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt} />
+                )
+            }
+        </div>
+    );
+
 }
